d5e145abb456a603600d2716a3ae00b9bd3ce746
Monica Dinculescu <noms@google.com>
2016-11-22 13:52:56 -0800

    use mariko's sw

--- sw.js
+++ sw.js
@@ -1,32 +1,64 @@
-var VERSION = '0.0.5';
+// From https://gist.github.com/kosamari/7c5d1e8449b2fbc97d372675f16b566e
 
-this.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(VERSION).then(cache => {
-    return cache.addAll([
-      '/font-style-matcher/',
-      '/font-style-matcher/index.html',
-      '/font-style-matcher/style.css',
-      '/font-style-matcher/app.js',
-      '/font-style-matcher/manifest.json',
-      '/font-style-matcher/images/favicon.ico',
-    ]).then(_ => this.skipWaiting());
-}))});
+var APP_PREFIX = 'FontStyleMatcher_';     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = '0.0.1';                    // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION;
+var URLS = [                            // Add URL you want to cache in this list.
+  '/font-style-matcher/',
+  '/font-style-matcher/index.html',
+  '/font-style-matcher/style.css',
+  '/font-style-matcher/app.js',
+  '/font-style-matcher/manifest.json',
+  '/font-style-matcher/images/favicon.ico',
+]
 
-this.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then((res) => {
-    // If there is no match in the cache, we get undefined back,
-    // in that case go to the network!
-    return res || fetch(e.request)
-  }));
-});
-
-this.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then((keys) => {
-    return Promise.all(keys.map(k => {
-      if (k !== VERSION) {
-        return caches.delete(k);
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
       }
-    })).then(_ => {
-      return this.clients.claim()
-    });
-}))});
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
